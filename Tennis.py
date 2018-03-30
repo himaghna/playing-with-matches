c97ce0cc267a047c24b9fcdc1d63ed5ec8f2c738
himaghna <himaghna@udel.edu>
2018-03-30 10:29:27 -0400

    Adding full folder import as a constructor option..

--- Tennis.py
+++ Tennis.py
@@ -24,7 +24,7 @@ class Tennis:
                 temporary_variable = Tennis()
                 for filename in glob.glob(os.path.join(path, '*.csv')):
                     print(filename)
-                    temporary_variable = temporary_variable + Tennis(path= filename)
+                    temporary_variable = temporary_variable + Tennis(path = filename)
                 self.data = temporary_variable.data
                 self.path = path
 
